42788493b861985de6580fa641672a16db17029f
Spina Nico <spinanico93@gmail.com>
2019-03-03 12:39:31 +0100

    version 0.0.1

--- src/App.js
+++ src/App.js
@@ -24,68 +24,93 @@ class App extends Component {
     this.my_place = `spaghetti 3.50$ \npizza 5,70$\nhamburger 6.8$
     `;
     this.state = {
-      float_comma: [],
-      float_dot:[],
-      integer:[],
 
-        int: true,
-        comma: true,
-        dot:true,
+      active_int: true,
+      active_float_comma: true,
+      active_float_dot: true,
 
-      sum_float_comma: 0,
-      sum_float_dot : 0,
-      sum_integer: 0,
-      sum_total: 0,
+      sum_total:0,
+      sum_float_comma:0,
+      sum_float_dot:0,
+      sum_int:0
     };
     this._editable = React.createRef();
     
+    this.re_for_float_with_dot = /\b\d+\.\d+\b/mgi;
+    this.re_for_float_with_comma = /\b\d+,\d+\b/mgi;
+    this.re_for_int = /\b\d+\b/mgi;
   }
 
+
+
+  calc_deciamal_with_comma(text){
+
+    let _array = text.match(this.re_for_float_with_comma);
+    if(_array === null || !this.state.active_float_comma){
+      _array = [];
+    }
+    let sum = 0;
+    for(let i of _array){
+      sum = sum + parseFloat(i.replace(",","."))
+    }
+
+    return {
+      array: _array,
+      sum: sum
+    }
+  }
+
+  calc_integer(text){
+    text = String(text.replace(this.re_for_float_with_comma, "").replace(this.re_for_float_with_dot, ""))
+
+    let _array = text.match(this.re_for_int);
+    if(_array === null || ! this.state.active_int){
+      _array = [];
+    }
+    let sum = 0;
+    for(let i of _array){
+      sum = sum + parseInt(i)
+    }
+
+    return {
+      array: _array,
+      sum: sum
+    }
+  }
+
+  calc_decimal_with_dot(text){
+    let _array = text.match(this.re_for_float_with_dot);
+    if(_array === null || !this.state.active_float_dot ){
+      _array = [];
+    }
+    let sum = 0;
+    for(let i of _array){
+      sum = sum + parseFloat(i)
+    }
+
+    return {
+      array: _array,
+      sum: sum
+    }
+  }
 
   search_on_text(){
     console.clear();
     console.log(this.state)
     let text = this._editable.current.value;
-    let [sum_float_comma, sum_float_dot, sum_integer, sum_total] = [0, 0, 0, 0];
-    
-    if (text === ""){
-      text = this.my_place;
-    }
 
-    let re_for_float = /\d+\.\d+/mgi;
-    let re_for_float_2 = /\b\d+,\d+\b/mgi;
-    let re_for_int = /\b\d+\b/mgi;
-    
-    let all_float_comma = text.match(re_for_float_2)===null?[]:text.match(re_for_float_2);
-    let all_float_dot = text.match(re_for_float)===null?[]:text.match(re_for_float);
-
-    let text_for_int = String(text.replace(re_for_float, "").replace(re_for_float_2, "")).match(re_for_int);
-
-
-
-    for(let i of this.state.float_comma){
-      sum_float_comma = sum_float_comma + parseFloat(i.replace(",","."))
-    }
-    for(let i of this.state.float_comma){
-      sum_float_dot = sum_float_comma + parseFloat(i)
-    }
-
-    for(let i of this.state.integer){
-      sum_integer = sum_integer + parseInt(i) 
-    }
-
-    sum_total = sum_float_comma + sum_float_dot + sum_integer
+    let result_float_comma = this.calc_deciamal_with_comma(text)
+    let result_flaot_dot = this.calc_decimal_with_dot(text)
+    let result_int = this.calc_integer(text)
 
     this.setState({
 
-      integer:this.state.int?text_for_int:[],
-      float_comma: this.state.comma?all_float_comma:[],
-      float_dot: this.state.dot?all_float_dot:[],
+      sum_float_comma: result_float_comma.sum,
+      sum_float_dot: result_flaot_dot.sum,
+      sum_int: result_int.sum,
 
-      sum_float_comma:sum_float_comma,
-      sum_float_dot: sum_float_dot,
-      sum_integer: sum_integer,
-      sum_total:sum_total
+      sum_total: result_flaot_dot.sum + result_float_comma.sum + result_int.sum
+
     });
    console.log(this.state)
   }
@@ -102,9 +127,22 @@ class App extends Component {
       <div className="box">
         <span className="title">What should I sum?</span><br/>
         <div className="container">
-          <button className={this.state.comma?"button":"button disabled"} onClick={()=>{this.setState({ comma:!this.state.comma});}}>decimal comma</button>
-          <button className={this.state.dot?"button":"button disabled"}  onClick={()=>{this.setState({ dot:!this.state.dot});}}>decimal dot</button>
-          <button className={this.state.int?"button":"button disabled"}  onClick={()=>{this.setState({ int:!this.state.int});}}>Integer</button>
+          <button className={this.state.active_float_comma?"button":"button disabled"} 
+          onClick={()=>{
+            this.setState({ 
+              active_float_comma:!this.state.active_float_comma
+            });
+            setTimeout(()=>this.search_on_text(), 20);
+            }
+          }>decimal comma</button>
+          <button className={this.state.active_float_dot?"button":"button disabled"}  onClick={()=>{
+            this.setState({ active_float_dot:!this.state.active_float_dot});
+            setTimeout(()=>this.search_on_text(), 20);
+            }}>decimal dot</button>
+          <button className={this.state.active_int?"button":"button disabled"}  onClick={()=>{
+            this.setState({ active_int:!this.state.active_int});
+            setTimeout(()=>this.search_on_text(), 20);
+            }}>Integer</button>
         </div>
       </div>
        <textarea 
@@ -119,7 +157,7 @@ class App extends Component {
           <div className="grid-output">
             <Result title="Sum of decimal with (,)" value={this.state.sum_float_comma}/>
             <Result title="Sum of decimal with (.)" value={this.state.sum_float_dot}/>
-            <Result title="Sum of integer" value={this.state.sum_integer}/>
+            <Result title="Sum of integer" value={this.state.sum_int}/>
             <Result title="total sum" value={this.state.sum_total}/>
           </div>
         </div>
